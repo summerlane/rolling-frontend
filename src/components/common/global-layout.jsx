88fafb94edcb3310e90d9c0be88fef3042e23b93
BZzzzi <khj23465@naver.com>
2025-11-17 18:44:14 +0900

    Rafactor: 헤더 만들기 버튼 링크 변경

--- src/components/common/global-layout.jsx
+++ src/components/common/global-layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet, useLocation } from "react-router";
 import Header from "@/components/common/header";
 
-const PAGES_WITH_BUTTON = ["main", "list"];
+const PAGES_WITH_BUTTON = ["/", "list"];
 
 export default function GlobalLayout() {
   const location = useLocation();
-  const showButton = PAGES_WITH_BUTTON.some((page) => location.pathname.includes(page));
+  const showButton = PAGES_WITH_BUTTON.some((page) =>
+    location.pathname.includes(page)
+  );
 
   return (
     <>
